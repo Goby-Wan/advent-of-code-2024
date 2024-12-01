5e9538b65459a2a823d0750260dd73fe3d3499f7
Goby-Wan <6798988+Goby-Wan@users.noreply.github.com>
2024-12-01 18:01:46 +0100

    Day_01 - Part_02

--- day-1-c#/goby-ex1.cs
+++ day-1-c#/goby-ex1.cs
@@ -5,33 +5,47 @@ namespace AdventOfCode
 {
     class Day_01
     {
-        private readonly string[] _inputOne,
-            _inputTwo;
+        private readonly string[] _input;
         private List<int> _listOne,
             _listTwo;
 
         public Day_01()
         {
-            _inputOne = File.ReadAllLines("input/day_01/input-ex1.txt");
+            _input = File.ReadAllLines("input/day_01/input-ex1.txt");
             _listOne = new List<int>();
             _listTwo = new List<int>();
 
-            Console.WriteLine("Résultat de l'exercice 1 : " + PartOne(_inputOne));
-        }
-
-        private int PartOne(string[] input)
-        {
-            int result = 0;
-
-            ParseInput(input);
+            ParseInput(_input);
             _listOne.Sort();
             _listTwo.Sort();
+
+            Console.WriteLine("Résultat de la partie 1 : " + PartOne());
+            Console.WriteLine("Résultat de la partie 2 : " + PartTwo());
+        }
+
+        private int PartOne()
+        {
+            int result = 0;
 
             for (int i = 0; i < _listOne.Count(); i++)
             {
                 result += Int32.Abs(_listOne[i] - _listTwo[i]);
             }
 
+            return result;
+        }
+
+        private int PartTwo()
+        {
+            int result = 0;
+
+            Dictionary<int, int> groupedValues = _listTwo
+                .GroupBy(i => i)
+                .ToDictionary(x => x.Key, x => x.Count());
+            foreach (int i in _listOne)
+            {
+                result += i * groupedValues.GetValueOrDefault(i, 0);
+            }
             return result;
         }
 
