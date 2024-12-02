0793fef38cc8902cb1b3130aeec07ecda808d4df
Alexandre GOBY <6798988+Goby-Wan@users.noreply.github.com>
2024-12-02 12:45:13 +0100

    Day_02 - Part_02

--- day-2-c#/goby-ex2.cs
+++ day-2-c#/goby-ex2.cs
@@ -6,57 +6,92 @@ namespace AdventOfCode
   class Day_02
   {
     private readonly string[] _input;
-    private List<int[]> _listReports;
+    private List<List<int>> _listReports;
     public Day_02()
     {
       _input = File.ReadAllLines("input/day_02/input-ex2.txt");
-      _listReports = new List<int[]>();
+      _listReports = new List<List<int>>();
       ParseInput(_input);
 
       Console.WriteLine("Résultat de la partie 1 : " + PartOne());
+      Console.WriteLine("Résultat de la partie 2 : " + PartTwo());
     }
 
     private int PartOne()
     {
       int result = 0;
 
-      foreach (int[] report in _listReports)
+      foreach (List<int> report in _listReports)
       {
-        Boolean isValidIncr = true;
-        Boolean isValidDecr = true;
-
-        // Test montant
-        for (int i = 1; i < report.Length; i++)
-        {
-          if (report[i] - report[i - 1] < 1 || report[i] - report[i - 1] > 3)
-          {
-            isValidIncr = false;
-            break;
-          }
-        }
-        // Test descendant
-        for (int i = 1; i < report.Length; i++)
-        {
-          if (report[i] - report[i - 1] > -1 || report[i] - report[i - 1] < -3)
-          {
-            isValidDecr = false;
-            break;
-          }
-        }
-
-        result += isValidIncr || isValidDecr ? 1 : 0;
+        result += TestReport(report) ? 1 : 0;
       }
 
       return result;
     }
+
+    private int PartTwo()
+    {
+      int result = 0;
+
+      foreach (List<int> report in _listReports)
+      {
+        if (TestReport(report))
+        {
+          result += 1;
+        }
+        else
+        {
+          for (int i = 0; i < report.Count; i++)
+          {
+            List<int> reportCopy = new List<int>(report);
+            reportCopy.RemoveAt(i);
+            if (TestReport(reportCopy))
+            {
+              result += 1;
+              break;
+            }
+          }
+        }
+      }
+
+      return result;
+    }
+
 
     private void ParseInput(string[] input)
     {
       foreach (string line in input)
       {
         string[] lineParsed = Regex.Split(line, " ");
-        _listReports.Add(lineParsed.Select(Int32.Parse).ToArray());
+        _listReports.Add(lineParsed.Select(Int32.Parse).ToList());
       }
+    }
+
+    private Boolean TestReport(List<int> report)
+    {
+      Boolean isValidIncr = true;
+      Boolean isValidDecr = true;
+
+      // Test montant
+      for (int i = 1; i < report.Count; i++)
+      {
+        if (report[i] - report[i - 1] is < 1 or > 3)
+        {
+          isValidIncr = false;
+          break;
+        }
+      }
+      // Test descendant
+      for (int i = 1; i < report.Count; i++)
+      {
+        if (report[i] - report[i - 1] is > -1 or < -3)
+        {
+          isValidDecr = false;
+          break;
+        }
+      }
+
+      return isValidIncr || isValidDecr;
     }
   }
 }
